d32653215fa486b584a5412ef9b685b3dabed6b9
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-03-16 18:40:33 +0100

    Added example IHDR chunk.

---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/Chunk.cs
@@ -0,0 +1,26 @@
+﻿namespace PNGAnalyzer
+{
+    public class Chunk
+    {
+        public Chunk(string type, byte[] data, int crc)
+        {
+            Type = type;
+            Data = data;
+            CRC = crc;
+        }
+
+        public string Type { get; }
+        public byte[] Data { get; }
+        public int CRC { get; }
+
+        public virtual string GetInfo()
+        {
+            return ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Type)}: {Type}\n {nameof(Data)}: {Data}\n {nameof(CRC)}: {CRC}";
+        }
+    }
+}
---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/IHDR.cs
@@ -0,0 +1,27 @@
+﻿namespace PNGAnalyzer
+{
+    public class IHDR : Chunk
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public byte BitDepth { get; }
+        public byte ColorType { get; }
+        public byte CompressionMethod { get; }
+        public byte FileterMethod { get; }
+        public byte InterlaceMethod { get; }
+
+        public IHDR(string type, byte[] data, int crc) : base(type, data, crc)
+        {
+        }
+
+        public override string GetInfo()
+        {
+            return base.GetInfo() + "\n" + ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}\n {nameof(Height)}: {Height}\n {nameof(BitDepth)}: {BitDepth}\n {nameof(ColorType)}: {ColorType}\n {nameof(CompressionMethod)}: {CompressionMethod}\n {nameof(FileterMethod)}: {FileterMethod}\n {nameof(InterlaceMethod)}: {InterlaceMethod}";
+        }
+    }
+}
