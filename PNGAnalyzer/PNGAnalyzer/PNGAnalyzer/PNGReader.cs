a682320ba68e3ad705e9488649b4dcfaf52b1da1
agngan <agnieszka.gano@gmail.com>
2020-03-16 15:20:19 +0100

    Added incomplete PNGReader class and PNGReaderTests.

---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/PNGReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PNGAnalyzer
+{
+    public class PNGReader
+    {
+        public static Dictionary<string, byte[]> Read(string filePath)
+        {
+            byte[] bytes = File.ReadAllBytes(filePath);
+            if (!IsPNG(bytes))
+                throw new FormatException("The file is not a PNG file!");
+            
+            Dictionary<string, byte[]> dictionary = new Dictionary<string, byte[]>();
+
+            return dictionary;
+        }
+
+        private static bool IsPNG(byte[] bytes)
+        {
+            return bytes.Take(8).SequenceEqual(new byte[]{137, 80, 78, 71, 13, 10, 26, 10});
+        }
+    }
+}
