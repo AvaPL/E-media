2e047152de2816b6f0fc395d9535054e46ca6d5b
agngan <agnieszka.gano@gmail.com>
2020-03-17 20:00:32 +0100

    Finished PLTE class.

--- PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/PLTE.cs
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/PLTE.cs
@@ -1,13 +1,31 @@
-﻿namespace PNGAnalyzer
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PNGAnalyzer
 {
     public class PLTE : Chunk
     {
-        public byte Red { get; }
-        public byte Green { get; }
-        public byte Blue { get; }
-
         public PLTE(string type, byte[] data, int crc) : base(type, data, crc)
         {
+            Entries = new List<Entry>(data.Length/3);
+            ParseData(data);
+        }
+
+        public PLTE(Chunk chunk) : base(chunk)
+        {
+            Entries = new List<Entry>(chunk.Data.Length/3);
+            ParseData(chunk.Data);
+        }
+
+        public List<Entry> Entries { get; }
+
+
+        private void ParseData(byte[] data)
+        {
+            for (int i = 0; i < data.Length; i += 3)
+            {
+                Entries.Add(new Entry(data[i], data[i + 1], data[i + 2]));
+            }
         }
 
         public override string GetInfo()
@@ -17,7 +35,21 @@
 
         public override string ToString()
         {
-            return $"{nameof(Red)}: {Red}\n {nameof(Green)}: {Green}\n {nameof(Blue)}: {Blue}";
+            return $"{nameof(Entries)}: {Entries}";
+        }
+
+        public class Entry
+        {
+            public Entry(byte red, byte green, byte blue)
+            {
+                Red = red;
+                Green = green;
+                Blue = blue;
+            }
+
+            public byte Red { get; }
+            public byte Green { get; }
+            public byte Blue { get; }
         }
     }
 }
