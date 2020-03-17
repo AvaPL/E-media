cbe168bb70721b512afafcb2f64f844af598d559
agngan <agnieszka.gano@gmail.com>
2020-03-17 19:01:48 +0100

    Finished cHRM class.

--- PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/cHRM.cs
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/cHRM.cs
@@ -2,17 +2,35 @@
 {
     public class cHRM : Chunk
     {
-        public int  WhitePointX { get; }
-        public int WhitePointY { get; }
-        public int RedX { get; }
-        public int RedY { get; }
-        public int GreenX { get; }
-        public int GreenY { get; }
-        public int BlueX { get; }
-        public int BlueY { get; }
-
         public cHRM(string type, byte[] data, int crc) : base(type, data, crc)
         {
+            ParseData(data);
+        }
+
+        public cHRM(Chunk chunk) : base(chunk)
+        {
+            ParseData(chunk.Data);
+        }
+
+        public int  WhitePointX { get; private set; }
+        public int WhitePointY { get; private set; }
+        public int RedX { get; private set; }
+        public int RedY { get; private set; }
+        public int GreenX { get; private set; }
+        public int GreenY { get; private set; }
+        public int BlueX { get; private set; }
+        public int BlueY { get; private set; }
+
+        private void ParseData(byte[] data)
+        {
+            WhitePointX = Converter.ToInt32(data, 0);
+            WhitePointY = Converter.ToInt32(data, 4);
+            RedX = Converter.ToInt32(data, 8);
+            RedY = Converter.ToInt32(data, 12);
+            GreenX = Converter.ToInt32(data, 16);
+            GreenY = Converter.ToInt32(data, 20);
+            BlueX = Converter.ToInt32(data, 24);
+            BlueY = Converter.ToInt32(data, 28);
         }
 
         public override string GetInfo()
@@ -22,7 +40,14 @@
 
         public override string ToString()
         {
-            return $"{nameof(WhitePointX)}: {WhitePointX}\n {nameof(WhitePointY)}: {WhitePointY}\n {nameof(RedX)}: {RedX}\n {nameof(RedY)}: {RedY}\n {nameof(GreenX)}: {GreenX}\n {nameof(GreenY)}: {GreenY}\n {nameof(BlueX)}: {BlueX}\n {nameof(BlueY)}: {BlueY}";
+            return $"{nameof(WhitePointX)}: {WhitePointX}\n" + 
+            $"{nameof(WhitePointY)}: {WhitePointY}\n" +
+            $"{nameof(RedX)}: {RedX}\n" +
+            $"{nameof(RedY)}: {RedY}\n" +
+            $"{nameof(GreenX)}: {GreenX}\n" +
+            $"{nameof(GreenY)}: {GreenY}\n" +
+            $"{nameof(BlueX)}: {BlueX}\n +" +
+            $"{nameof(BlueY)}: {BlueY}";
         }
     }
 }
