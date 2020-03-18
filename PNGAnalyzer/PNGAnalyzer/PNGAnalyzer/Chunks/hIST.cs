666ada88b135f96995390352eaf617864a7299a7
agngan <agnieszka.gano@gmail.com>
2020-03-18 13:12:22 +0100

    Finished hIST class. Added methods to Converter.

--- PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/hIST.cs
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/hIST.cs
@@ -6,6 +6,20 @@
 
         public hIST(string type, byte[] data, int crc) : base(type, data, crc)
         {
+            ParseData(data);
+        }
+
+        public hIST(Chunk chunk) : base(chunk)
+        {
+            ParseData(chunk.Data);
+        }
+
+        private void ParseData(byte[] data)
+        {
+            for (int i = 0; i < data.Length; i+=2)
+            {
+                Histogram[i / 2] = Converter.ToInt16(data, i);
+            }
         }
 
         public override string GetInfo()
--- PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Support/Converter.cs
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Support/Converter.cs
@@ -10,12 +10,26 @@ namespace PNGAnalyzer
                 Array.Reverse(value, 0, value.Length);
             return BitConverter.ToInt32(value, 0);
         }
-        
+
         public static int ToInt32(byte[] value, int startIndex)
         {
             if (BitConverter.IsLittleEndian)
                 Array.Reverse(value, startIndex, 4);
             return BitConverter.ToInt32(value, startIndex);
+        }
+
+        public static short ToInt16(byte[] value)
+        {
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(value, 0, value.Length);
+            return BitConverter.ToInt16(value, 0);
+        }
+
+        public static short ToInt16(byte[] value, int startIndex)
+        {
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(value, startIndex, 2);
+            return BitConverter.ToInt16(value, startIndex);
         }
     }
 }
