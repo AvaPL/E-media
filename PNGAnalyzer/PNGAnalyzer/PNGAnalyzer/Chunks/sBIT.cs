82ed209cb396709bea74f28fcdaef4442ca2cf3f
agngan <agnieszka.gano@gmail.com>
2020-03-18 13:24:43 +0100

    Finished sBIT class.

--- PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/sBIT.cs
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/sBIT.cs
@@ -2,10 +2,23 @@
 {
     public class sBIT : Chunk
     {
-        public byte[] SignificantBytes { get; }
-
         public sBIT(string type, byte[] data, int crc) : base(type, data, crc)
         {
+            SignificantBytes = new byte[data.Length];
+            ParseData(data);
+        }
+
+        public sBIT(Chunk chunk) : base(chunk)
+        {
+            SignificantBytes = new byte[chunk.Data.Length];
+            ParseData(chunk.Data);
+        }
+
+        public byte[] SignificantBytes { get; private set; }
+
+        private void ParseData(byte[] data)
+        {
+            SignificantBytes = data;
         }
 
         public override string GetInfo()
