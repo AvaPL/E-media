047c37a2e602306a511623a200a5f015814c502e
agngan <agnieszka.gano@gmail.com>
2020-03-18 13:17:19 +0100

    Improved hIST class.

--- PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/hIST.cs
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/hIST.cs
@@ -2,21 +2,23 @@
 {
     public class hIST : Chunk
     {
-        public short[] Histogram { get; }
-
         public hIST(string type, byte[] data, int crc) : base(type, data, crc)
         {
+            Histogram = new short[data.Length / 2];
             ParseData(data);
         }
 
         public hIST(Chunk chunk) : base(chunk)
         {
+            Histogram = new short[chunk.Data.Length / 2];
             ParseData(chunk.Data);
         }
 
+        public short[] Histogram { get; }
+
         private void ParseData(byte[] data)
         {
-            for (int i = 0; i < data.Length; i+=2)
+            for (int i = 0; i < data.Length; i += 2)
             {
                 Histogram[i / 2] = Converter.ToInt16(data, i);
             }
