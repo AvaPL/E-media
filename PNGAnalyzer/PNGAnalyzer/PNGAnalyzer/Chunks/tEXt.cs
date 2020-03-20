3ff8b47ed820d365820210489a9bbd0b0fc6747c
agngan <agnieszka.gano@gmail.com>
2020-03-20 18:49:08 +0100

    Added tEXt class.

---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/tEXt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+
+namespace PNGAnalyzer
+{
+    public class tEXt : Chunk
+    {
+        public tEXt(string type, byte[] data, int crc) : base(type, data, crc)
+        {
+            if (type != "tEXt")
+                throw new ArgumentException("Invalid chunk type passed to tEXt");
+            ParseData(data);
+        }
+
+        public tEXt(Chunk chunk) : base(chunk)
+        {
+            if (chunk.Type != "tEXt")
+                throw new ArgumentException("Invalid chunk type passed to tEXt");
+            ParseData(chunk.Data);
+        }
+
+        public string Keyword { get; set; }
+        public string Text { get; set; }
+
+        private void ParseData(byte[] data)
+        {
+            Encoding latin1 = Encoding.GetEncoding(28592);
+            Keyword = latin1.GetString(data.TakeWhile(b => b != 0).ToArray());
+            Text = latin1.GetString(data.Skip(Keyword.Length + 1).ToArray());
+        }
+
+        public override string GetInfo()
+        {
+            return base.GetInfo() + "\n" + ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Keyword)}: {Keyword}\n" +
+                   $"{nameof(Text)}: {Text}";
+        }
+    }
+}
