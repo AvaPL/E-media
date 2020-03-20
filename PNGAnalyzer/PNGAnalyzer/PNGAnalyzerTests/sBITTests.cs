adb298fb16783bd113f880ffedf5084a4dce729c
agngan <agnieszka.gano@gmail.com>
2020-03-20 20:25:49 +0100

    Added sBIT tests.

---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzerTests/sBITTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using PNGAnalyzer;
+
+namespace PNGAnalyzerTests
+{
+    [TestFixture]
+    public class sBITTests
+    {
+        [Test]
+        public void ShouldReadsBIT()
+        {
+            string filePath = @"../../Data/lena.png";
+            sBIT sbit = new sBIT(PNGReader.Read(filePath)[1]);
+            Assert.AreEqual("sBIT", sbit.Type);
+            Assert.AreEqual(8, sbit.SignificantBytes[0]);
+            Assert.AreEqual(3, sbit.SignificantBytes.Length);
+            
+        }
+    }
+}
