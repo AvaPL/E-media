a2f50d728af05f731fee17ee18b80c3737bd43c4
Bartosz Starosta <bstarosta77@gmail.com>
2020-03-20 14:46:33 +0100

    Updated gAMA, sRGB and IEND classes

--- PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/IEND.cs
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/IEND.cs
@@ -1,7 +1,19 @@
-﻿namespace PNGAnalyzer
+﻿using System;
+
+namespace PNGAnalyzer
 {
-    public class IEND
+    public class IEND : Chunk
     {
-        
+        public IEND(string type, byte[] data, int crc) : base(type, data, crc)
+                 {
+                     if(type != "IEND")
+                         throw new ArgumentException("Invalid chunk type passed to IEND");
+                 }
+         
+                 public IEND(Chunk chunk) : base(chunk)
+                 {
+                     if (chunk.Type != "IEND")
+                         throw new ArgumentException("Invalid chunk type passed to IEND");
+                 }
     }
 }
--- PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/sRGB.cs
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/sRGB.cs
@@ -1,7 +1,38 @@
-﻿namespace PNGAnalyzer
+﻿using System;
+
+namespace PNGAnalyzer
 {
-    public class sRGB
+    public class sRGB : Chunk
     {
+        public sRGB(string type, byte[] data, int crc) : base(type, data, crc)
+        {
+            if(type != "sRGB")
+                throw new ArgumentException("Invalid chunk type passed to sRGB");
+            ParseData(data);
+        }
+
+        public sRGB(Chunk chunk) : base(chunk)
+        {
+            if (chunk.Type != "sRGB")
+                throw new ArgumentException("Invalid chunk type passed to sRGB");
+            ParseData(chunk.Data);
+        }
+
+        public byte RenderingIntent { get; private set; }
+
+        private void ParseData(byte[] data)
+        {
+            RenderingIntent = data[0];
+        }
         
+        public override string GetInfo()
+        {
+            return base.GetInfo() + "\n" + ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(RenderingIntent)}: {RenderingIntent}\n";
+        }
     }
 }
