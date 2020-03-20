cd806135ef3322d53a802262649e44b6491b85b2
Bartosz Starosta <bstarosta77@gmail.com>
2020-03-20 17:41:54 +0100

    Added tIME and zTXt, without tests.

---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/tIME.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PNGAnalyzer
+{
+    public class tIME : Chunk
+    {
+        public tIME(string type, byte[] data, int crc) : base(type, data, crc)
+        {
+            if(type != "tIME")
+                throw new ArgumentException("Invalid chunk type passed to tIME");
+            ParseData(data);
+        }
+
+        public tIME(Chunk chunk) : base(chunk)
+        {
+            if (chunk.Type != "tIME")
+                throw new ArgumentException("Invalid chunk type passed to tIME");
+            ParseData(chunk.Data);
+        }
+
+        public DateTime LatestModificationDate { get; private set; }
+
+        private void ParseData(byte[] data)
+        {
+            LatestModificationDate = 
+                new DateTime(Converter.ToInt16(data), data[2], data[3], data[4], data[5], data[6]);
+        }
+
+        public override string GetInfo()
+        {
+            return base.GetInfo() + "\n" + ToString();
+        }
+        
+        public override string ToString()
+        {
+            return LatestModificationDate.ToString("dd/MM/yyyy HH:mm:ss")+"\n";
+        }
+    }
+}
---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/zTXt.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace PNGAnalyzer
+{
+    public class zTXt : Chunk
+    {
+        public zTXt(string type, byte[] data, int crc) : base(type, data, crc)
+        {
+            if(type != "zTXt")
+                throw new ArgumentException("Invalid chunk type passed to zTXt");
+            ParseData(data);
+        }
+
+        public zTXt(Chunk chunk) : base(chunk)
+        {
+            if (chunk.Type != "zTXt")
+                throw new ArgumentException("Invalid chunk type passed to zTXt");
+            ParseData(chunk.Data);
+        }
+
+        public string Keyword { get; private set; }
+        public byte CompressionMethod { get; private set; }
+        public byte[] CompressedText { get; private set; }
+
+        private void ParseData(byte[] data)
+        {
+            Keyword = GetKeyword(data);
+            int index = Keyword.Length + 1;
+            CompressionMethod = data[index];
+            index += 1;
+            CompressedText = data.Skip(index).ToArray();
+        }
+        
+        private string GetKeyword(byte[] data)
+        {
+            int nullSeparatorIndex = Array.IndexOf(data, 0, 0);
+            return Encoding.GetEncoding("ISO-8859-1").GetString(data, 0, nullSeparatorIndex);
+        }
+        
+        public override string GetInfo()
+        {
+            return base.GetInfo() + "\n" + ToString();
+        }
+        
+        public override string ToString()
+        {
+            return $"{nameof(Keyword)}: {Keyword}\n";
+        }
+    }
+}
