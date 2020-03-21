b47e3fdeec1fba9235f553dd45ba693e48a24db9
agngan <agnieszka.gano@gmail.com>
2020-03-21 18:31:02 +0100

    Added tEXt test.

--- PNGAnalyzer/PNGAnalyzer/PNGAnalyzerTests/tEXtTests.cs
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzerTests/tEXtTests.cs
@@ -7,12 +7,22 @@ namespace PNGAnalyzerTests
     public class tEXtTests
     {
         [Test]
-        public void ShouldReadtEXt()
+        public void ShouldReadtEXtFromLena()
         {
             string filePath = @"../../../Data/lena2.png";
             tEXt text = new tEXt(PNGReader.Read(filePath)[5]);
             Assert.AreEqual("tEXt", text.Type);
             Assert.AreEqual("date:create", text.Keyword);
+        }
+        
+        [Test]
+        public void ShouldReadtEXtFromItxt()
+        {
+            string filePath = @"../../../Data/itxt.png";
+            tEXt text = new tEXt(PNGReader.Read(filePath)[7]);
+            Assert.AreEqual("tEXt", text.Type);
+            Assert.AreEqual("Title", text.Keyword);
+            Assert.AreEqual("PNG", text.Text);
         }
     }
 }
