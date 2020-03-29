99d94f4ee4e3f7913c1532c287aa813b8789a529
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-03-29 17:30:51 +0200

    Added UI template.

---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzerUI/MainWindow.Designer.cs
@@ -0,0 +1,123 @@
+﻿using System.ComponentModel;
+
+namespace PNGAnalyzerUI
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FilepathTextBox = new System.Windows.Forms.TextBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.ChunksButton = new System.Windows.Forms.Button();
+            this.AnonymizeButton = new System.Windows.Forms.Button();
+            this.FourierTransformButton = new System.Windows.Forms.Button();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // FilepathTextBox
+            // 
+            this.FilepathTextBox.Location = new System.Drawing.Point(12, 12);
+            this.FilepathTextBox.Name = "FilepathTextBox";
+            this.FilepathTextBox.ReadOnly = true;
+            this.FilepathTextBox.Size = new System.Drawing.Size(383, 23);
+            this.FilepathTextBox.TabIndex = 0;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(401, 12);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(71, 23);
+            this.BrowseButton.TabIndex = 1;
+            this.BrowseButton.Text = "Browse...";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // ChunksButton
+            // 
+            this.ChunksButton.Enabled = false;
+            this.ChunksButton.Location = new System.Drawing.Point(12, 41);
+            this.ChunksButton.Name = "ChunksButton";
+            this.ChunksButton.Size = new System.Drawing.Size(149, 23);
+            this.ChunksButton.TabIndex = 2;
+            this.ChunksButton.Text = "Chunks";
+            this.ChunksButton.UseVisualStyleBackColor = true;
+            this.ChunksButton.Click += new System.EventHandler(this.ChunksButton_Click);
+            // 
+            // AnonymizeButton
+            // 
+            this.AnonymizeButton.Enabled = false;
+            this.AnonymizeButton.Location = new System.Drawing.Point(167, 41);
+            this.AnonymizeButton.Name = "AnonymizeButton";
+            this.AnonymizeButton.Size = new System.Drawing.Size(150, 23);
+            this.AnonymizeButton.TabIndex = 3;
+            this.AnonymizeButton.Text = "Anonymize";
+            this.AnonymizeButton.UseVisualStyleBackColor = true;
+            this.AnonymizeButton.Click += new System.EventHandler(this.AnonymizeButton_Click);
+            // 
+            // FourierTransformButton
+            // 
+            this.FourierTransformButton.Enabled = false;
+            this.FourierTransformButton.Location = new System.Drawing.Point(323, 41);
+            this.FourierTransformButton.Name = "FourierTransformButton";
+            this.FourierTransformButton.Size = new System.Drawing.Size(149, 23);
+            this.FourierTransformButton.TabIndex = 4;
+            this.FourierTransformButton.Text = "Fourier transform";
+            this.FourierTransformButton.UseVisualStyleBackColor = true;
+            this.FourierTransformButton.Click += new System.EventHandler(this.FourierTransformButton_Click);
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.DefaultExt = "png";
+            this.OpenFileDialog.Filter = "PNG files (*.png)|*.png|All files (*.*)|*.*";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 76);
+            this.Controls.Add(this.FourierTransformButton);
+            this.Controls.Add(this.AnonymizeButton);
+            this.Controls.Add(this.ChunksButton);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.FilepathTextBox);
+            this.Name = "MainWindow";
+            this.Text = "PNG Analyzer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FourierTransformButton;
+        private System.Windows.Forms.Button AnonymizeButton;
+        private System.Windows.Forms.Button ChunksButton;
+        private System.Windows.Forms.TextBox FilepathTextBox;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+    }
+}
