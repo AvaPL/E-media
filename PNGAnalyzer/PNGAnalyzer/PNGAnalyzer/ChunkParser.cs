e75bb5192e08439114a8a6ee489fdc526358fa5d
agngan <agnieszka.gano@gmail.com>
2020-03-30 13:59:46 +0200

    Cleaned code.

--- PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/ChunkParser.cs
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/ChunkParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PNGAnalyzer
 {
@@ -7,50 +8,32 @@ namespace PNGAnalyzer
     {
         public static List<Chunk> Parse(List<Chunk> chunks)
         {
-            List<Chunk> result = new List<Chunk>();
-            foreach (var chunk in chunks)
-            {
-                switch (chunk.Type)
-                {
-                    case "bKGD": result.Add(new bKGD(chunk));
-                        break;
-                    case "cHRM": result.Add(new cHRM(chunk));
-                        break;
-                    case "gAMA": result.Add(new gAMA(chunk));
-                        break;
-                    case "hIST": result.Add(new hIST(chunk));
-                        break;
-                    case "iCCP": result.Add(new iCCP(chunk));
-                        break;
-                    case "IDAT": result.Add(new IDAT(chunk));
-                        break;
-                    case "IEND": result.Add(new IEND(chunk));
-                        break;
-                    case "IHDR": result.Add(new IHDR(chunk));
-                        break;
-                    case "iTXt": result.Add(new iTXt(chunk));
-                        break;
-                    case "pHYs": result.Add(new pHYs(chunk));
-                        break;
-                    case "PLTE": result.Add(new PLTE(chunk));
-                        break;
-                    case "sBIT": result.Add(new sBIT(chunk));
-                        break;
-                    case "sPLT": result.Add(new sPLT(chunk));
-                        break;
-                    case "sRGB": result.Add(new sRGB(chunk));
-                        break;
-                    case "tEXt": result.Add(new tEXt(chunk));
-                        break;
-                    case "tIME": result.Add(new tIME(chunk));
-                        break;
-                    case "zTXt": result.Add(new zTXt(chunk));
-                        break;
-                    default: throw new FormatException("Invalid chunk type.");
-                }
-            }
+            return chunks.Select(ParseChunk).ToList();
+        }
 
-            return result;
+        private static Chunk ParseChunk(Chunk chunk)
+        {
+            return chunk.Type switch
+            {
+                "bKGD" => new bKGD(chunk),
+                "cHRM" => new cHRM(chunk),
+                "gAMA" => new gAMA(chunk),
+                "hIST" => new hIST(chunk),
+                "iCCP" => new iCCP(chunk),
+                "IDAT" => new IDAT(chunk),
+                "IEND" => new IEND(chunk),
+                "IHDR" => new IHDR(chunk),
+                "iTXt" => new iTXt(chunk),
+                "pHYs" => new pHYs(chunk),
+                "PLTE" => new PLTE(chunk),
+                "sBIT" => new sBIT(chunk),
+                "sPLT" => new sPLT(chunk),
+                "sRGB" => new sRGB(chunk),
+                "tEXt" => new tEXt(chunk),
+                "tIME" => new tIME(chunk),
+                "zTXt" => new zTXt(chunk),
+                _ => throw new FormatException("Invalid chunk type.")
+            };
         }
     }
 }
