b35dc933a3bbec485946fa6ab0f4c72d3411a57b
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-04-03 17:47:20 +0200

    Added eXIf chunk parsing.

--- PNGAnalyzer/PNGAnalyzer/PNGAnalyzerTests/Chunks/eXIfTests.cs
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzerTests/Chunks/eXIfTests.cs
@@ -12,17 +12,25 @@ namespace PNGAnalyzerTests
         {
             string filePath = @"../../../Data/sinusExif.png";
             List<Chunk> chunks = PNGFile.Read(filePath);
-            eXIf exif = new eXIf(chunks[chunks.Count-2]);
+            eXIf exif = new eXIf(chunks[chunks.Count - 2]);
             Assert.AreEqual("eXIf", exif.Type);
         }
-        
+
         [Test]
         public void ShouldContainCorrectEndianFlag()
         {
             string filePath = @"../../../Data/sinusExif.png";
             List<Chunk> chunks = PNGFile.Read(filePath);
-            eXIf exif = new eXIf(chunks[chunks.Count-2]);
+            eXIf exif = new eXIf(chunks[chunks.Count - 2]);
             Assert.AreEqual("MM", exif.EndianFlag);
+        }
+
+        [Test]
+        public void ShouldReadIFD()
+        {
+            string filePath = @"../../../Data/kostkiExif.png";
+            List<Chunk> chunks = PNGFile.Read(filePath);
+            eXIf exif = new eXIf(chunks[chunks.Count - 2]);
         }
     }
 }
