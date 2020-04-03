4509687a5c855f174dc698a24f0e4af45f237656
Bartosz Starosta <bstarosta77@gmail.com>
2020-04-03 14:09:40 +0200

    Added eXIf chunk prototype and some basic tests

---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/Chunks/eXIf.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PNGAnalyzer
+{
+    public class eXIf : Chunk
+    {
+        public eXIf(string type, byte[] data, uint crc) : base(type, data, crc)
+        {
+            if (type != "eXIf")
+                throw new ArgumentException("Invalid chunk type passed to eXIf");
+            ParseData(data);
+        }
+
+        public eXIf(Chunk chunk) : base(chunk)
+        {
+            if (chunk.Type != "eXIf")
+                throw new ArgumentException("Invalid chunk type passed to eXIf");
+            ParseData(chunk.Data);
+        }
+
+        public string EndianFlag { get; private set; }
+
+
+        private void ParseData(byte[] data)
+        {
+            EndianFlag = Encoding.GetEncoding("ISO-8859-1").GetString(data.Take(2).ToArray());
+            int index = 2;
+            if (Converter.ToInt16(data, index)!=42)
+            {
+                throw new FormatException("eXIf data is not in TIFF format");
+            }
+            index += 2;
+        }
+
+
+        public class IFD
+        {
+            public class Tag
+            {
+                public short TagID { get; private set; }
+                public short TagType { get; private set; }
+                public uint Count { get; private set; }
+                public uint Offset { get; private set; }
+            }
+            
+            public List<Tag> Tags { get; private set; }
+            public short Length { get; private set; }
+        }
+    }
+}
---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzerTests/Chunks/eXIfTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using PNGAnalyzer;
+
+namespace PNGAnalyzerTests
+{
+    [TestFixture]
+    public class eXIfTests
+    {
+        [Test]
+        public void ShouldReadeXIf()
+        {
+            string filePath = @"../../../Data/sinusExif.png";
+            List<Chunk> chunks = PNGFile.Read(filePath);
+            eXIf exif = new eXIf(chunks[chunks.Count-2]);
+            Assert.AreEqual("eXIf", exif.Type);
+        }
+        
+        [Test]
+        public void ShouldContainCorrectEndianFlag()
+        {
+            string filePath = @"../../../Data/sinusExif.png";
+            List<Chunk> chunks = PNGFile.Read(filePath);
+            eXIf exif = new eXIf(chunks[chunks.Count-2]);
+            Assert.AreEqual("MM", exif.EndianFlag);
+        }
+    }
+}
