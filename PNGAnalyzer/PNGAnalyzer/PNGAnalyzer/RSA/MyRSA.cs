be2f05fe53e44bd759be4b0d717a58fb534a9a68
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-05-04 20:54:25 +0200

    Implemented key generation, sometimes fails to calculate modulus for some reason.

--- PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/RSA/MyRSA.cs
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/RSA/MyRSA.cs
@@ -1,7 +1,109 @@
-﻿namespace PNGAnalyzer.RSA
+﻿using System.Security.Cryptography;
+using Extreme.Mathematics;
+
+namespace PNGAnalyzer.RSA
 {
-    public class MyRSA
+    public class MyRSA : IRSA
     {
-        
+        public byte[] Encrypt(byte[] data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ImportParameters(RSAParameters parameters)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        RSAParameters IRSA.GenerateKeyPair(int numberOfBits)
+        {
+            return GenerateKeyPair(numberOfBits);
+        }
+
+        public static RSAParameters GenerateKeyPair(int numberOfBits)
+        {
+            BigInteger
+                primeMin = new BigInteger(6074001000) <<
+                           (numberOfBits / 2 - 33); // primeMin ≈ √2 × 2^(bits - 1), assures key length
+            BigInteger p = RandomPrimeAbove(primeMin, numberOfBits / 2);
+            BigInteger q = RandomPrimeAbove(primeMin, numberOfBits / 2);
+            BigInteger n = p * q; // Modulus
+            BigInteger lcm = IntegerMath.LeastCommonMultiple(p - 1, q - 1);
+            BigInteger e = RandomCoprimeBelow(lcm); // Exponent
+            BigInteger d = BigInteger.ModularInverse(e, lcm);
+            // Remaining in RSAParameters
+            BigInteger dp = d % (p - 1);
+            BigInteger dq = d % (q - 1);
+            BigInteger inverseQ = BigInteger.ModularInverse(q, p);
+            return new RSAParameters
+            {
+                D = d.ToByteArray(),
+                DP = dp.ToByteArray(),
+                DQ = dq.ToByteArray(),
+                Exponent = e.ToByteArray(),
+                InverseQ = inverseQ.ToByteArray(),
+                Modulus = n.ToByteArray(),
+                P = p.ToByteArray(),
+                Q = q.ToByteArray()
+            };
+        }
+
+        private static BigInteger RandomPrimeAbove(BigInteger bound, int numberOfBits)
+        {
+            BigInteger result;
+            do
+            {
+                result = RandomPrime(numberOfBits);
+            } while (result <= bound);
+
+            return result;
+        }
+
+        private static BigInteger RandomPrime(int numberOfBits)
+        {
+            BigInteger result;
+            do
+            {
+                result = RandomBigInteger(numberOfBits);
+            } while (!result.IsPseudoPrime(256));
+
+            return result;
+        }
+
+        private static BigInteger RandomBigInteger(int numberOfBits)
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] bytes = new byte[numberOfBits / 8 + 1]; // Include sign bit
+            rng.GetBytes(bytes);
+            bytes[bytes.Length - 1] &= 0x7F; // Force sign bit to positive
+            return new BigInteger(bytes);
+        }
+
+        private static BigInteger RandomCoprimeBelow(BigInteger bound)
+        {
+            BigInteger result;
+            do
+            {
+                result = RandomPrimeBelow(bound);
+            } while (bound % result == 0);
+
+            return result;
+        }
+
+        private static BigInteger RandomPrimeBelow(BigInteger bound)
+        {
+            BigInteger result;
+            do
+            {
+                result = RandomPrime(bound.BitCount);
+            } while (result >= bound);
+
+            return result;
+        }
     }
 }
---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzerTests/MyRSATests.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using NUnit.Framework;
+using PNGAnalyzer.RSA;
+
+namespace PNGAnalyzerTests
+{
+    [TestFixture]
+    public class MyRSATests
+    {
+        [Test]
+        public void ShouldGenerateKeyOfSpecifiedLength()
+        {
+            int[] keyLengths = {512 /*, 1024, 2048, 4096*/};
+            foreach (var keyLength in keyLengths)
+            {
+                RSAParameters rsaParameters = MyRSA.GenerateKeyPair(keyLength);
+                Assert.AreEqual(keyLength + 2 * 8 /* additional sign bytes from p and q */,
+                    rsaParameters.Modulus.Length * 8);
+            }
+        }
+    }
+}
