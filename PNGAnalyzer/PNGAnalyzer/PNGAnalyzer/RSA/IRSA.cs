b842b6d4f82aef22a407fde1560be32a1f4aedc5
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-05-04 16:33:32 +0200

    Added IRSA interface and KeyPair class.

---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/RSA/IRSA.cs
@@ -0,0 +1,12 @@
+﻿namespace PNGAnalyzer.RSA
+{
+    public interface IRSA
+    {
+        byte[] Encrypt(byte[] data);
+        byte[] Decrypt(byte[] data);
+        void ImportPublicKey(string publicKey);
+        void ImportPrivateKey(string privateKey);
+        void ImportKeyPair(KeyPair keyPair);
+        KeyPair GenerateKeyPair(int numberOfBits);
+    }
+}
---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/RSA/KeyPair.cs
@@ -0,0 +1,8 @@
+﻿namespace PNGAnalyzer.RSA
+{
+    public class KeyPair
+    {
+        public string PublicKey { get; set; }
+        public string PrivateKey { get; set; }
+    }
+}
