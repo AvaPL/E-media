5d6043967bd584eb01dcc3020186cfbf78fb2cba
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-05-04 12:45:34 +0200

    Added IDATs concatenation to bytes.

---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzer/IDATConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PNGAnalyzer
+{
+    public class IDATConverter
+    {
+        public static byte[] ConcatToBytes(List<IDAT> idats)
+        {
+            long bytesCount = idats.Select(idat => idat.Data.Length).Sum();
+            byte[] result = new byte[bytesCount];
+            long resultIndex = 0;
+            foreach (var idat in idats)
+            {
+                idat.Data.CopyTo(result, resultIndex);
+                resultIndex += idat.Data.Length;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PNGAnalyzer/PNGAnalyzer/PNGAnalyzerTests/IDATConverterTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using PNGAnalyzer;
+
+namespace PNGAnalyzerTests
+{
+    [TestFixture]
+    public class IDATConverterTests
+    {
+        [Test]
+        public void ShouldConcatEmptyList()
+        {
+            List<IDAT> idats = new List<IDAT>();
+            byte[] result = IDATConverter.ConcatToBytes(idats);
+            Assert.AreEqual(new byte[] {}, result);
+        }
+        
+        [Test]
+        public void ShouldConcatOneIDAT()
+        {
+            IDAT idat = new IDAT("IDAT", new byte[] {1, 5, 7}, 0);
+            List<IDAT> idats = new List<IDAT> {idat};
+            byte[] result = IDATConverter.ConcatToBytes(idats);
+            Assert.AreEqual(new byte[] {1, 5, 7}, result);
+        }
+
+        [Test]
+        public void ShouldConcatFourIDATs()
+        {
+            List<IDAT> idats = new List<IDAT>
+            {
+                new IDAT("IDAT", new byte[] {1, 5, 7}, 0),
+                new IDAT("IDAT", new byte[] {4}, 0),
+                new IDAT("IDAT", new byte[] {}, 0),
+                new IDAT("IDAT", new byte[] {2, 5}, 0)
+            };
+            byte[] result = IDATConverter.ConcatToBytes(idats);
+            Assert.AreEqual(new byte[] {1, 5, 7, 4, 2, 5}, result);
+        }
+    }
+}
